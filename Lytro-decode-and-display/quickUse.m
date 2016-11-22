f4b82be8d94043734677f6d00677a97ea7d64cf2
F Zhou <F Zhou@vdesktop.MMCLab.com>
2016-11-22 20:57:37 +0800

    a decoded file

---
+++ Lytro-decode-and-display/quickUse.m
@@ -0,0 +1,5 @@
+LFMatlabPathSetup;
+LFUtilProcessWhiteImages('Cameras\');
+LFUtilDecodeLytroFolder('Images/Illum/Lorikeet.lfp');
+load('E:\LFToolbox0.3_Samples1\Images\Illum\Lorikeet__Decoded.mat');
+LFDispMousePan(LF);
